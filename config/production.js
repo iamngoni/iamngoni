c36acb4628fd86a8b19934376f7185ff45dbe260
iamngoni <el.nick.ngonie@gmail.com>
2020-08-24 10:27:36 +0200

    added timestamp

--- config/production.js
+++ config/production.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: 'production',
-  db: 'mongodb+srv://ngonidzashe:2Z5gM4tEOCIaKvqv@cluster0-uirhn.mongodb.net/production?retryWrites=true&w=majority',
+  db: 'mongodb+srv://ngonidzashe:2Z5gM4tEOCIaKvqv@cluster0-uirhn.mongodb.net/iamngoni?retryWrites=true&w=majority',
   port: process.env.PORT,
   message: 'Connected to MongoAtlas Database'
 }
--- models/tiles.js
+++ models/tiles.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose')
 
 var tile = mongoose.Schema({
 	username: String,
-	score: Number
+	score: Number,
+	timestamp: {
+		type: Date,
+		default: Date.now
+	}
 })
 
 module.exports = mongoose.model('HallOfFame', tile)
